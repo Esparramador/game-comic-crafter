63882ba60587f374c601da7edc05e480b7bda167
Joan Sadia gil <sadiagiljoan@gmail.com>
2026-03-06 14:34:03 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import { createPageUrl } from "@/utils";
-import { Link } from "react-router-dom";
 
 export default function Home() {
   const [showSplash, setShowSplash] = useState(true);
