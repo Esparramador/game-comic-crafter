e6965764ac73f65b6604ea02ee18d064b3bacb2d
Joan Sadia gil <sadiagiljoan@gmail.com>
2026-03-06 20:37:44 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -51,6 +51,7 @@ import HomeScreen from './pages/HomeScreen';
 import Landing from './pages/Landing';
 import Error401 from './pages/Error401';
 import Error403 from './pages/Error403';
+import Error500 from './pages/Error500';
 import __Layout from './Layout.jsx';
 
 
@@ -59,6 +60,7 @@ export const PAGES = {
     "Landing": Landing,
     "Error401": Error401,
     "Error403": Error403,
+    "Error500": Error500,
 }
 
 export const pagesConfig = {
